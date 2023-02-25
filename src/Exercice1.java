38179e667439c9455b729f8c7d12e78b5b309011
Reyad <riadox41@gmail.com>
2023-02-25 22:41:05 +0100

    Updated Exercice1 file

--- src/Exercice1.java
+++ src/Exercice1.java
@@ -1,10 +1,18 @@
+
 public class Exercice1 {
+
     // I x = new I();
+    // faux,
+    // on n’instancie pas une interface
 
     // C1 y = new C1();
-    // I[] z = { new C1(), new C2() };
-    // C1 w = new C2();
-
-    // i.faux,
+    // faux,
     // on n’instancie pas une interface
+
+    // I[] z = { new C1(), new C2() };
+    // idem, même si deux classes différentes. Elles implémentent la même interface
+
+    // C1 w = new C2();
+    // faux, on déclare une classe et on instancie avec une autre (types
+    // incompatibles)
 }
