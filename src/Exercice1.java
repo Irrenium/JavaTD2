3c2235e29e6fe09c6f38d182b8e9db442a2704bf
Reyad <riadox41@gmail.com>
2023-02-25 22:35:59 +0100

    Created Exercice1 file

---
+++ src/Exercice1.java
@@ -0,0 +1,10 @@
+public class Exercice1 {
+    // I x = new I();
+
+    // C1 y = new C1();
+    // I[] z = { new C1(), new C2() };
+    // C1 w = new C2();
+
+    // i.faux,
+    // on n’instancie pas une interface
+}
